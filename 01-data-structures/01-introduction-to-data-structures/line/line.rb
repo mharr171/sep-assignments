18f11c67c85653ce08240174bfbd30a094757e4c
Matthew Harris <mharr171.z@gmail.com>
2018-07-31 23:51:48 -0700

    01 code work

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,37 @@ class Line
   end
 
   def join(person)
+    members.push(person)
   end
 
   def leave(person)
+    members.delete(person)
   end
 
   def front
+    members.first
   end
 
   def middle
+    if (members.length % 2 === 0)
+      members[members.length/2]
+    else
+      members[(members.length+1)/2]
+    end
   end
 
   def back
+    members.last
   end
 
   def search(person)
+    members.include?(person) ? person : nil
   end
 
   private
 
   def index(person)
+    members.index(person)
   end
 
 end
--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,27 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Array.new(width) {Array.new(height)}
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    matrix[x][y] = pixel
   end
 
   def at(x, y)
+    return inbounds(x,y) ? matrix[x][y] : nil
   end
 
   private
 
   def inbounds(x, y)
+    return (x < width) &&
+            (x > 0) &&
+            (y < height) &&
+            (y > 0)
   end
 
 end
